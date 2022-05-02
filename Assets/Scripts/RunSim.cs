040e20874e20c8f4557573fc875acd1018d2bf65
Karthik Palusa <Kpalusa@rams.colostate.edu>
2022-05-02 16:52:18 -0600

    RunSim Script

---
+++ Assets/Scripts/RunSim.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunSim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Turn off all buttons
+// GameObject.Find('center-button').SetActive(true);
+// GameObject.Find('top-right-button').SetActive(false);
+// GameObject.Find('bottom-right-button').SetActive(false);
+// GameObject.Find('top-left-button').SetActive(false);
+// GameObject.Find('bottom-left-button').SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
